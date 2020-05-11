0788ea768adbb4cf896576fa3739954642d774df
ayush bansal <ayu023ban@gmail.com>
2020-05-11 22:17:18 +0530

    cleared the clutter of views

--- bug_reporter/views.py
+++ bug_reporter/views.py
@@ -1,21 +1,11 @@
 from rest_framework import generics, status, viewsets ,request
 from rest_framework.parsers import FileUploadParser,MultiPartParser
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
-from rest_framework.decorators import action, permission_classes
+from rest_framework.decorators import permission_classes
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication,TokenAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 from bug_reporter.permissions import *
 from bug_reporter import models , serializers
-from django.http import Http404
 from django_filters import rest_framework as filters
-from django.db.models.signals import post_save
-from django.conf import settings
-from django.dispatch import receiver
-from django.urls import path
 from django.shortcuts import get_object_or_404
-# from django.db.models.signals import *
 
 # Create your views here.
 class UserList(generics.ListAPIView):
@@ -25,19 +15,6 @@ class UserList(generics.ListAPIView):
     permission_classes = [CustomAuthentication]
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
-    # def create(self, request, *args, **kwargs):
-        
-    #     return super().create(request, *args, **kwargs)
-
-
-# for user in models.User.objects.all():
-#     Token.objects.get_or_create(user=user)
-
-# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-# def create_auth_token(sender, instance=None, created=False, **kwargs):
-#     if created:
-#         Token.objects.create(user=instance)
-
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     # permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
@@ -64,13 +41,6 @@ class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Project.objects.all()
     serializer_class = serializers.ProjectDetailSerializer
     permission_classes = (CustomAuthentication,IsTeamMember,IsCreatorOfObject)
-
-
-# class ProjectUpdate(generics.UpdateAPIView):
-#     queryset = models.Project.objects.all()
-#     serializer_class = serializers.ProjectDetailSerializer()
-#     permission_classes = (IsTeamMember,)
-
 
 class CommentDetail(generics.ListCreateAPIView):
     queryset=models.Comment.objects.all()
@@ -110,33 +80,3 @@ class ImageList(generics.ListCreateAPIView):
 class BugDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Bug.objects.all()
     serializer_class = serializers.BugUpdateSerializer
-
-
-# class SignUp(generics.CreateAPIView):
-#     queryset = models.User.objects.all()
-#     serializer_class = serializers.SignUpSerializer
-#     permission_classes = (IsAuthenticatedOrCreate,)
-
-
-# class ProjectViewSet(viewsets.ModelViewSet):
-#     queryset = models.Project.objects.all()
-#     serializer_class = serializers.ProjectSerializer
-#     filter_backends = (filters.DjangoFilterBackend,)
-#     filterset_fields = ['created_at']
-
-# class BugViewSet(viewsets.ModelViewSet):
-#     queryset = models.Bug.objects.all()
-#     serializer_class = serializers.BugSerializer
-#     # @action(method = ['post'])
-#     def create(self,request):
-#         serializer = serializers.BugSerializer(data = request.data)
-#         if(serializer.is_valid()):
-            
-#             bugData = {"project":request.data["project"],"user":request.data["user"],"name":request.data["name"],"tag":request.data["tag"],"status":request.data["status"]}
-#             bugSerializer = serializers.BugSerializer(data = bugData)
-#             bugSerializer.save()            
-#             print(bugData)
-#             print(request.data)
-#             imageSerializer = serializers.ImageSerializer(data = [])
-#             return Response(data=request.data)
-#         return Response(data=serializer.errors,status = status.HTTP_400_BAD_REQUEST)
